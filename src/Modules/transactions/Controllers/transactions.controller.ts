7e99b0e1e96d9433027ea1cd06c30c062b1e8501
André Fernandes <andreluissilvafernandes@gmail.com>
2025-04-03 11:38:38 -0300

    feat(transaction controller): adicionado rota para retornar estatisticas

--- src/Modules/transactions/Controllers/transactions.controller.ts
+++ src/Modules/transactions/Controllers/transactions.controller.ts
@@ -6,20 +6,23 @@ import {
   UsePipes,
   ValidationPipe,
   Delete,
+  Get,
 } from '@nestjs/common';
 
 import { CreateTransctionDto } from '../Application/createTransaction/CreateTransaction.dto';
 import { CreateTransactionUseCase } from '../Application/createTransaction/CreateTransaction.useCase';
 import { DeleteTransactionUseCase } from '../Application/deleteTransaction/DeleteTransaction.useCase';
+import { AnalisysTransactiosUseCase } from '../Application/analisysTransactions/AnalisysTransactions.useCase';
 
-@Controller('transacao')
+@Controller('')
 export class TransactionController {
   constructor(
     private readonly createTransactionUseCase: CreateTransactionUseCase,
     private readonly deleteTransactionUseCase: DeleteTransactionUseCase,
+    private readonly analisysTransactionUseCase: AnalisysTransactiosUseCase,
   ) {}
 
-  @Post('')
+  @Post('transacao')
   @UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
   async createTransaction(@Body() data: CreateTransctionDto) {
     try {
@@ -35,12 +38,26 @@ export class TransactionController {
     return;
   }
 
-  @Delete('')
+  @Delete('transacao')
   @UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
   async deleteTransactions() {
     try {
       await this.deleteTransactionUseCase.execute();
       return { statusCode: 200, message: 'Transactions deleted successfully' };
+    } catch (error: unknown) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'An unknown error occurred';
+      throw new ConflictException(errorMessage);
+    }
+  }
+
+  @Get('estatistica')
+  @UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
+  async getAnalisysTransactions() {
+    try {
+      const returnedTransaction =
+        await this.analisysTransactionUseCase.execute();
+      return returnedTransaction;
     } catch (error: unknown) {
       const errorMessage =
         error instanceof Error ? error.message : 'An unknown error occurred';
