b3d7be516cd687cf8ddcf3769c36f3dd9c07b8cf
André Fernandes <andreluissilvafernandes@gmail.com>
2025-04-03 10:20:13 -0300

    feat(transaction module): adicionado use case de deletar transação na module

--- src/Modules/transactions/Modules/transactions.module.ts
+++ src/Modules/transactions/Modules/transactions.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionController } from '../Controllers/transactions.controller';
 import { TransactionService } from '../Services/transactions.service';
 import { CreateTransactionUseCase } from '../Application/createTransaction/CreateTransaction.useCase';
+import { DeleteTransactionUseCase } from '../Application/deleteTransaction/DeleteTransaction.useCase';
 
 @Module({
   imports: [],
   controllers: [TransactionController],
-  providers: [TransactionService, CreateTransactionUseCase],
+  providers: [
+    TransactionService,
+    CreateTransactionUseCase,
+    DeleteTransactionUseCase,
+  ],
 })
 export class TransactionModule {}
