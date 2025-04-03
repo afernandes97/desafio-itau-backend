dc2f26d50bd8c37896c615dacf04d0c2161cc963
André Fernandes <andreluissilvafernandes@gmail.com>
2025-04-03 19:36:29 -0300

    fix(app module): fixado import health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionModule } from './Modules/transactions/Modules/transactions.module';
-import { HealthModule } from './Modules/dashboard copy/Modules/health.module';
+import { HealthModule } from './Modules/health/Modules/health.module';
 
 @Module({
   imports: [TransactionModule, HealthModule],
